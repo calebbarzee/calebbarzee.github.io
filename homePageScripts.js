1e0eee694ae331dbcfd158ed205c44cb597bcd13
calebbarzee <barzeec@gmail.com>
2021-01-13 17:18:48 -0700

    js and practice files

--- homePageScripts.js
+++ homePageScripts.js
@@ -1,3 +1,19 @@
 // JavaScript Document
-.getElementById("copyrightYear").textContent = 
-.getElementById("updateDate").textContent = 
+
+//function copyrightDate() {
+let options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+};
+let copyrightYear = new Date.toLocaleDateString("en-US", options);
+print(copyrightYear);
+document.getElementById("copyrightYear").textContent = copyrightYear;
+//}
+
+//function lastUpdatedDate() {
+let lastUpdated = Date(document.lastModified);
+print(lastUpdated);
+document.getElementById("updateDate").textContent = lastUpdated;
+//}
