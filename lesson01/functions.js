cbe1ea3f8d742707ab8427ed469df4d7f5424abb
calebbarzee <barzeec@gmail.com>
2021-09-16 09:29:40 -0600

    Class Activity Completion

--- lesson01/functions.js
+++ lesson01/functions.js
@@ -1,3 +1,11 @@
-// alert("This is a test of the alert system");
+/* This script edits a bulleted list */
 
-console.log("This is a test of the console");
+function modify_list() {
+    let new_li = document.querySelector('#new_li').value
+    // .value grabs the actual value of an input box
+    //.querySelector can be used for both #id and .class
+    let create_li = document.createElement('li');
+    //.createElement creates a new tag/element in DOM
+    create_li.innerHTML = new_li;
+    document.querySelector('#bulleted_list').appendChild(create_li);
+}
