16a52553b3cf167d2bccd2902d1b68522a7e5e2d
calebbarzee <barzeec@gmail.com>
2021-10-18 22:01:48 -0600

    responsive menu

--- lesson04/js/responsive_menu.js
+++ lesson04/js/responsive_menu.js
@@ -5,5 +5,14 @@ menu_button.addEventListener('click',function() {
 		mainnav.classList.toggle('responsive')
 		},false);
 
+menu_button.addEventListener('mouseover', function( event ) {
+	event.target.src = "assests/hamburger_menu_black-01.png";
+setTimeout(function() {
+		event.target.src = "assests/hamburger_menu.png";
+	  }, 500);
+	}, false);
+
 window.onresize = function() {
-	if(window.innerWidth > 760) 		mainnav.classList.remove('responsive')};
+	if(window.innerWidth > 760) 
+		mainnav.classList.remove('responsive')};
+
