042da3260ba7539cd4a8cae22c9be24ada99afb7
calebbarzee <barzeec@gmail.com>
2021-10-29 23:48:35 -0600

    header weather page update

--- lesson04/js/responsive_menu.js
+++ lesson04/js/responsive_menu.js
@@ -1,18 +1,25 @@
 const menu_button = document.querySelector('.menu_button');
 const mainnav = document.querySelector('.navigation')
 
+
 menu_button.addEventListener('click',function() {
 		mainnav.classList.toggle('responsive')
 		},false);
 
-menu_button.addEventListener('mouseover', function( event ) {
-	event.target.src = "assets/hamburger_menu_black-01.png";
-setTimeout(function() {
-		event.target.src = "assets/hamburger_menu.png";
-	  }, 500);
-	}, false);
-
 window.onresize = function() {
 	if(window.innerWidth > 760) 
 		mainnav.classList.remove('responsive')};
+		
+menu_button.addEventListener('mouseover', change_to_black ,false);
 
+menu_button.addEventListener('mouseout', change_to_white ,false);
+
+function change_to_black() {
+	menu_button.removeAttribute("src", "assets/hamburger_menu.png");
+	menu_button.setAttribute("src", "assets/hamburger_menu_black-01.png");
+}
+
+function change_to_white() {
+	menu_button.removeAttribute("src", "assets/hamburger_menu_black-01.png");
+	menu_button.setAttribute("src", "assets/hamburger_menu.png");
+}
