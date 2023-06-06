81500c17aa66589d06ee02e809dc737142f6e781
Caleb Barzee <barzeec@gmail.com>
2023-06-06 10:27:26 -0600

    deployment fixes

--- next.config.js
+++ next.config.js
@@ -1,2 +1,11 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
 module.exports = withNextra()
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+   output: 'export',
+ };
+  
+ module.exports = nextConfig;
