f081c6fa4341dfb1aadb880dfd8836aa251a4e58
Caleb Barzee <barzeec@gmail.com>
2023-06-16 18:13:28 -0600

    design gallery complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from './pages/Layout';
 import Home from './pages/Home';
-// import Design from './pages/Design';
+import Design from './pages/Design';
 // import Development from './pages/Development';
 // import Resume from './pages/Resume';
 import NoPage from './pages/NoPage';
@@ -12,8 +12,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="" element={<Home />} />
-          {/* <Route path="design" element={<Design />} />
-          <Route path="development" element={<Development />} />
+          <Route path="design" element={<Design />} />
+          {/* <Route path="development" element={<Development />} />
           <Route path="resume" element={<Resume />} /> */}
           <Route path="*" element={<NoPage />} />
         </Route>
