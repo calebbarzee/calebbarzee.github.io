872ac38870f87514acf65a52c4a1df28cd685ed7
calebbarzee <barzeec@gmail.com>
2024-03-14 12:12:26 -0600

    conform to eslint, no unescaped char

--- app/page.js
+++ app/page.js
@@ -9,15 +9,15 @@ export default function Home() {
         <h1 className="text-3xl">Simple Solutions, Big Impact</h1>
         <br />
         <p className="text-md">
-          Hi there! I'm Caleb, an engineer who loves to solve big problems with
+          Hi there! I&apos;m Caleb, an engineer who loves to solve big problems with
           simple code.
         </p>
         <br />
         <h3 className="text-xl">A Bit About Me</h3>
         <p className="text-sm">
-          I've always been curious about how things work, leading me from
+          I&apos;ve always been curious about how things work, leading me from
           playful explorations in my backyard to becoming a passionate software
-          engineer. When I'm not coding, you can find me getting my hands dirty
+          engineer. When I&apos;m not coding, you can find me getting my hands dirty
           with pottery or gardening, cycling, expressing thoughts through
           design, or spending quality time with my family. These passions fuel
           and inspire my work.
@@ -26,7 +26,7 @@ export default function Home() {
         <h3 className="text-xl">My Work</h3>
         <p className="text-sm">
           As a specialist in Full-Stack Development, I focus on making things
-          work better for everyone. Whether it's using Rust for its reliability
+          work better for everyone. Whether it&apos;s using Rust for its reliability
           or dabbling in JavaScript, Python, C++, and Swift, I pick the best
           tool for the job.
         </p>
@@ -37,16 +37,16 @@ export default function Home() {
         <br />
         <h3 className="text-xl">Making a Difference</h3>
         <p className="text-sm">
-          I've helped make marketplaces more trustworthy, city services more
+          I&apos;ve helped make marketplaces more trustworthy, city services more
           accessible, and student learning more effective. My work is all about
           creating something that matters, reducing complexity, and improving
           lives.
         </p>
         <br />
-        <h3 className="text-xl">Let's Chat</h3>
+        <h3 className="text-xl">Let&apos;s Chat</h3>
         <p className="text-sm">
-          If you're into creating tech that helps people or just want to share
-          your latest DIY project, I'd love to hear from you.
+          If you&apos;re into creating tech that helps people or just want to share
+          your latest DIY project, I&apos;d love to hear from you.
         </p>
         <br />
         <div className="mx-auto grid grid-cols-4 grid-rows-4 gap-2">
