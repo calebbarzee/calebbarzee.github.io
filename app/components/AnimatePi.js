2bfeab97379d0e2ce7ee3af1d5aef799112c401b
calebbarzee <barzeec@gmail.com>
2024-03-13 23:10:01 -0600

    clear digits when numOfDigits change

--- app/components/AnimatePi.js
+++ app/components/AnimatePi.js
@@ -27,7 +27,7 @@ export default function AnimatePi({PI}) {
 	  return (
 		<div className="flex flex-col gap-4 p-6 md:grid md:grid-cols-5 md:grid-rows-5 md:gap-4 min-h-3 items-center justify-between max-w-full">
 		  <label className="md:row-start-1 md:col-start-1 md:col-span-2">Number of Digits: </label>
-		  <input className="bg-transparent border-2 border-slate-300 p-2 rounded-md md:row-start-1 md:col-start-3" title="Iterations" type="number" value={numOfDigits} onChange={(e) => {setNumOfDigits(e.target.value); setIsRunning(false)}} />
+		  <input className="bg-transparent border-2 border-slate-300 p-2 rounded-md md:row-start-1 md:col-start-3" title="Iterations" type="number" value={numOfDigits} onChange={(e) => {setNumOfDigits(e.target.value); setIsRunning(false); setDisplayedDigits('3.')}} />
 		  <button className="self-center md:self-auto hover:underline md:row-start-1" onClick={() => setIsRunning(!isRunning)}>{isRunning ? "Stop" : "Start"}</button>
 		  <div className="border-2 border-slate-300 p-4 rounded-md md:row-start-2 md:col-span-5 w-full">
 			<span className="text-2xl font-mono font-semibold break-words text-wrap">
